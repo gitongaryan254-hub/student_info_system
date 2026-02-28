0ecb923f8b8018e2decb389048290d45b6f0c024
Your Name <you@example.com>
2026-02-28 23:35:55 +0300

    Initial commit

---
+++ final question 1.py
@@ -0,0 +1,6 @@
+# variables queation one
+name = "Ryan Gitonga Githua"
+age = 19
+status = "AI and Cybersecurity student"
+print(f"hello, my name is {name}.I am {age} years old and current status is {status}.")
+
